7b72c7751c89645780def607735657b5e4619886
TESTER <120787636+Riders004@users.noreply.github.com>
2024-08-11 18:16:29 +0530

    Create work.js

---
+++ work.js
@@ -0,0 +1,28 @@
+const config = require('../../config');
+const {
+    anya
+} = require('../lib');
+const axios = require('axios');
+
+//༺─────────────────────────────────────
+
+anya(
+        {
+            name: "test",
+            alias: ['t'],
+            react: "✨",
+            need: "text",
+            category: "Baileys",
+            desc: "6o create gc and add owner",
+            cooldown: 8,
+            filename: __filename
+        },
+    async (anyaV2, pika, { args }) => {
+        if (!args[0]) return pika.reply("_❕Enter some texts to create!_");
+        const { key } = await pika.keyMsg("```Creating...```");
+        const group = await anyaV2.groupCreate(text, [config.ownernumber + "@s.whatsapp.net])
+console.log ("created group with id: " + group.gid)
+anyaV2.sendMessage(group.id, { text: 'hello there' }) // say hello to everyone on the group
+});
+
+//༺─────────────────────────────────────

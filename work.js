07651ec04ed6e05112d845fe9dea45f007f672a1
TESTER <120787636+Riders004@users.noreply.github.com>
2024-08-11 18:39:55 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,31 +1,23 @@
 const config = require('../../config');
-const {
-    anya
-} = require('../lib');
-const axios = require('axios');
-
-//༺─────────────────────────────────────
+const { anya } = require('../lib');
 
 anya(
-        {
-            name: "test",
-            alias: ['t'],
-            react: "✨",
-            need: "text",
-            category: "Baileys",
-            desc: "6o create gc and add owner",
-            cooldown: 8,
-            filename: __filename
-        },
+    {
+        name: 'test',
+        alias: ['t'],
+        react: '✨',
+        need: 'text',
+        category: 'Baileys',
+        desc: 'Create a group and add an owner',
+        cooldown: 8,
+        filename: __filename,
+    },
     async (anyaV2, pika, { args }) => {
-        if (!args[0]) return pika.reply("_❕Enter some texts to create!_");
-        const text = args.join(" ");
-        const { key } = await pika.keyMsg("```Creating...```");
-        const group = await anyaV2.groupCreate(text, [config.ownernumber + "@s.whatsapp.net"])
-        console.log(group);
-console.log ("created group with id: " + group.gid)
-        pika.reply(group.toString)
-anyaV2.sendMessage(group.id, { text: 'hello there' }) // say hello to everyone on the group
-});
-
-//༺─────────────────────────────────────
+        if (!args[0]) return pika.reply('_❕Enter some text to create!_');
+        const text = args.join(' ');
+        const group = await anyaV2.groupCreate(text, [config.ownernumber + '@s.whatsapp.net']);
+        console.log('Created group with ID:', group);
+        pika.reply(group.toString());
+      //  anyaV2.sendMessage(group.gid, { text: 'Hello there' }); // Say hello to everyone in the group
+    }
+);
